e412f26b0ccba133641082362141f6149752abfe
Austin Huang <im@austinhuang.me>
2018-09-15 12:24:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const { Plugin } = require('elements')
+const { Plugin } = require('elements'),
+      rpc = require('discord-rich-presence'),
+      fs = require('fs'),
+      settings = JSON.parse(fs.readFileSync("./settings.json", "utf8"));
 
 module.exports = class commands extends Plugin {
-  /**
-   * Contains all the loading logic, that does not depend on the DOM or other plugins
-   */
   preload () {}
-  /**
-   * Contains all the loading logic, that does depend on the DOM or other plugins
-   */
-  load () {}
-  /**
-   * Stuff to do on unload (e.g. freeing resources, timers and event handlers)
-   */
-  unload () {}
+  load () {
+    this.registerSettingsTab('Rich Presence', require('./settings.jsx'))
+  }
+  unload () {
+    rpc(settings.id)();
+  }
 }
