cb6634c9bbab22e50a49c9fd93dee8f623995a91
Austin Huang <im@austinhuang.me>
2018-09-15 14:44:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const { Plugin } = require('elements'),
 
 module.exports = class RPC extends Plugin {
   preload () {
+  }
+  load () {
     let a = this.settings,
         client = drpc(this.settings.id);
     delete a.id;
     a.instance = true;
     client.updatePresence(a);
-  }
-  load () {
     this.registerSettingsTab('Rich Presence', require('./settings.jsx'))
   }
   unload () {
     let client = drpc(this.settings.id);
-    client.updatePresence(a);
+    client.updatePresence();
   }
   settingsChanged () {
     let a = this.settings,
         client = drpc(this.settings.id);
     delete a.id;
     a.instance = true;
+    console.log(a);
     client.updatePresence(a);
   }
 }
